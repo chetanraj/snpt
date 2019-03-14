ce800699d09ce996f6d34a2a544c204edc7e29b4
Chetan Raj <chetan.narian@gmail.com>
2019-03-14 22:58:24 +0530

    Changes 1. Copy to clipboard 2. Added setState and state 3. Add precommit linting

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,8 +1,8 @@
-const __ReactImp = `import React from 'react';`;
-const __ReactImpDOM = `import ReactDOM from 'react-dom';`;
-const __ReactImpComponent = `import React, { Component } from 'react';`;
+const __ReactImp = `import React from 'react';`
+const __ReactImpDOM = `import ReactDOM from 'react-dom';`
+const __ReactImpComponent = `import React, { Component } from 'react';`
 const __ReactImpPropTypes = `import React, { Component } from 'react';
-import PropTypes from 'prop-types';`;
+import PropTypes from 'prop-types';`
 const __ReactComponent = `import React, { Component } from 'react'
 
 export default class Snpt extends Component {
@@ -13,22 +13,29 @@ export default class Snpt extends Component {
       </div>
     )
   }
-}`;
+}`
 const __ComponentDidMount = `componentDidMount = () => {
   
-}`;
-const __Vue = `import Vue from 'vue';`;
+}`
+const __state = `this.state = {}`
+const __setState = `this.setState({ 
+  key: value
+})`
+
+const __Vue = `import Vue from 'vue';`
 
 export const categories = {
-    'import': {
-      'react': __ReactImp,
-      'react-dom': __ReactImpDOM,
-      'react-component': __ReactImpComponent,
-      'react-prop-types': __ReactImpPropTypes,
-      'vue': __Vue
-    },
-    'react': {
-      'react-component': __ReactComponent,
-      'componentDidMount': __ComponentDidMount
-    },
+  import: {
+    react: __ReactImp,
+    'react-dom': __ReactImpDOM,
+    'react-component': __ReactImpComponent,
+    'react-prop-types': __ReactImpPropTypes,
+    vue: __Vue
+  },
+  react: {
+    'react-component': __ReactComponent,
+    componentDidMount: __ComponentDidMount,
+    state: __state,
+    setState: __setState
   }
+}
--- src/components/Snippets.js
+++ src/components/Snippets.js
@@ -1,28 +1,47 @@
 import React, { Component } from 'react'
 
 export default class Snippets extends Component {
+  setCode(code) {
+    //! Set the code to clipboard
+    this.copyToClipboard(code)
+    this.props.onClick(code)
+  }
 
-    render() {
-        return (
-            <>
-                {Object.keys(this.props.categories).map((category) => {
-                    return <div key={Math.random()}>
-                        <h2 key={Math.random()}>{category}</h2>
-                        {Object.keys(this.props.categories[category]).map(type => {
-                            console.log(type);
+  copyToClipboard(code) {
+    const textField = document.createElement('textarea')
+    textField.innerText = code
+    document.body.appendChild(textField)
+    textField.select()
+    document.execCommand('copy')
+    textField.remove()
+  }
 
-                            return <button
-                                id={type}
-                                key={Math.random()}
-                                className="btn-white"
-                                onClick={() => this.props.onClick(this.props.categories[category][type])}
-                            >
-                                {type}
-                            </button>
-                        })}
-                    </div>
-                })}
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        {Object.keys(this.props.categories).map(category => {
+          return (
+            <div key={Math.random()}>
+              <h2 key={Math.random()}>{category}</h2>
+              {Object.keys(this.props.categories[category]).map(type => {
+                console.log(type)
+
+                return (
+                  <button
+                    id={type}
+                    key={Math.random()}
+                    className="btn-white"
+                    onClick={() =>
+                      this.setCode(this.props.categories[category][type])
+                    }>
+                    {type}
+                  </button>
+                )
+              })}
+            </div>
+          )
+        })}
+      </>
+    )
+  }
 }
