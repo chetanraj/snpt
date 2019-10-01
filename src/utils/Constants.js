5a546636c395c8d56a0cbb0637843ddd6df4561b
Nabin Adhikari <nabin@stridesolutions.com.au>
2019-10-01 16:08:25 +1000

    Added some redux snpts

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -50,6 +50,91 @@ const __setState = `this.setState({ })`;
  */
 const __ReduxImp = `import { connect } from 'react-redux';`;
 
+const __ReduxCombineReducersImport = `import { combineReducers } from 'redux';`;
+const __ReduxCreateStoreImport = `import { createStore } from 'redux';`;
+
+const __ReduxConfigureStore = `import { combineReducers, createStore } from 'redux';
+import reducer1 from './reducer1';
+import reducer2 from './reducer2';
+
+export default function configureStore (initialState = {}) {
+    const reducers = {
+        reducer1: reducer1,
+        reducer2: reducer2,
+    };
+
+    const rootReducer = combineReducers({
+        ...reducers,
+        // Add any other if needed like routing.
+    });
+
+    return createStore(rootReducer, initialState);
+}`;
+
+const __ReduxAddToReact = `import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+import App from './components/App';
+
+const initialState = {};
+const store = configureStore(initialState);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('app')
+);`;
+
+const __ReduxSimpleActionCreator = `const incrementCountType = 'INCREMENT_COUNT';
+const decrementCountType = 'DECREMENT_COUNT';
+
+const initialState = { count: 0 };
+
+export const actionCreators = {
+  increment: () => ({ type: incrementCountType }),
+  decrement: () => ({ type: decrementCountType })
+};`;
+
+const __ReduxSimpleReducer = `const incrementCountType = 'INCREMENT_COUNT';
+const decrementCountType = 'DECREMENT_COUNT';
+
+const initialState = { count: 0 };
+
+export const actionCreators = {
+  increment: () => ({ type: incrementCountType }),
+  decrement: () => ({ type: decrementCountType })
+};
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case incrementCountType: return { ...state, count: state.count + 1 };
+    case decrementCountType: return { ...state, count: state.count - 1 };
+  }
+  return state || initialState;
+};`;
+
+const __ReduxSimpleContainer = `import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { actionCreators } from '../store/Counter';
+
+const Counter = props => (
+  <div>
+    <h1>Counter</h1>
+
+    <p>Current count: <strong>{props.count}</strong></p>
+
+    <button onClick={props.increment}>Increment</button>
+  </div>
+);
+
+export default connect(
+  state => state.counter,
+  dispatch => bindActionCreators(actionCreators, dispatch)
+)(Counter);`;
+
 /*
  * Vue
  */
@@ -170,6 +255,29 @@ export const types = {
     import: {
       redux: {
         code: __ReduxImp
+      },
+      combineReducer: {
+        code: __ReduxCombineReducersImport
+      },
+      createStore: {
+        code: __ReduxCreateStoreImport
+      }
+    },
+    configure: {
+      configureStore: {
+        code: __ReduxConfigureStore
+      },
+      addToReact: {
+        code: __ReduxAddToReact
+      },
+      simpleActionCreator: {
+        code: __ReduxSimpleActionCreator
+      },
+      simpleReducer: {
+        code: __ReduxSimpleReducer
+      },
+      simpleContainer: {
+        code: __ReduxSimpleContainer
       }
     }
   },
