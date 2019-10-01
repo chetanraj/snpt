67a33fb611c64f243d5265f2e3dd46fd696603c9
Erivaldo Dantas <erivaldo.dantas85@gmail.com>
2019-10-01 14:22:00 -0300

    Includes useEffect in snippets, resolve issue #15

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -45,6 +45,25 @@ const __constructorWithState = `constructor(props) {
 const __state = `this.state = {}`;
 const __setState = `this.setState({ })`;
 
+const __useEffect = `
+import React, { useEffect } from 'react';
+
+useEffect(()=>{
+  //replace the componentDidMount
+}, []);
+
+useEffect(()=>{
+  //replace the componentDidUpdate listening updated at "yourVariableOrState"
+}, [yourVariableOrState]);
+
+useEffect(()=>{
+  return () => {
+    //replace componentWillUnmount
+  } 
+});
+
+`;
+
 /*
  * Redux
  */
@@ -261,6 +280,10 @@ export const types = {
       },
       componentWillUnmount: {
         code: __ComponentWillUnMount,
+        type: 'react'
+      },
+      useEffect: {
+        code: __useEffect,
         type: 'react'
       }
     }
