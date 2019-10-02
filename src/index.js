b44b38a6a9405b0011fa9a85e4f7a7f6f4e7d55b
Leandro Kimura <leandrokk@jmalucelliti.com.br>
2019-10-02 15:52:24 -0300

    Register serviceWorker
    Able to register SW in this application

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.register();
