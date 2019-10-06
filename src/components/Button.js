e874f29e2026a92e5793ed1f2ef4ec3c51af0cca
Chetan Raj <chetan.narian@gmail.com>
2019-10-06 10:01:27 +0530

    Changes: Converted Button to styled-component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  border: 0 none;
+  background: #fff;
+  padding: 1em;
+  border-radius: 4px;
+  font-size: 16px;
+  margin: 0.4em;
+  cursor: pointer;
+  outline: none;
+`;
 
 const Button = props => (
-  <button
-    className="btn-white"
-    id={props.label}
-    key={Math.random()}
-    onClick={props.onClick}>
+  <StyledButton id={props.label} key={Math.random()} onClick={props.onClick}>
     {props.label}
-  </button>
+  </StyledButton>
 );
 
 export default Button;
