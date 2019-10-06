d04811cce8706b35ed16fd8cc28313a69adfcacf
Chetan Raj <chetan.narian@gmail.com>
2019-10-06 11:47:59 +0530

    Changes: Intro to Context API

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,13 @@ import Highlight, { defaultProps } from 'prism-react-renderer';
 import * as Sentry from '@sentry/browser';
 
 //! Other Components
+import Header from './Header';
 import Snippets from './Snippets';
+import { SnptProvider } from './SnptContext';
 import Notification from './Notification';
+
 import { frameworks, types } from '../utils/Constants';
+
 import '../scss/snpt.scss';
 import dracula from 'prism-react-renderer/themes/nightOwl';
 
@@ -28,6 +32,7 @@ class App extends Component {
     super(props);
 
     this.state = {
+      frameworks,
       framework: 'react',
       code: defaultCode,
       types: []
@@ -47,55 +52,50 @@ class App extends Component {
   }
 
   render() {
+    const frameworkContext = { framework: 'react', frameworks };
+
     return (
-      <div className="snpt">
-        <Notification onRef={ref => (this.notification = ref)} />
-        <div className="container">
-          <div className="snippets">
-            <header>
-              <ul className="frameworks">
-                {frameworks.map(framework => {
-                  return (
-                    <li
-                      className={
-                        this.state.framework === framework ? 'active' : ''
-                      }
-                      onClick={() => this.setFramework(framework)}
-                      key={framework}>
-                      {framework}
-                    </li>
-                  );
-                })}
-              </ul>
-            </header>
-            <Snippets
-              onClick={this.setCode.bind(this)}
-              types={types[this.state.framework]}
-            />
-            <footer id="footer">{this.state.footerMsg}</footer>
+      <SnptProvider value={frameworkContext}>
+        <div className="snpt">
+          <Notification onRef={ref => (this.notification = ref)} />
+          <div className="container">
+            <div className="snippets">
+              <Header onClick={this.setFramework.bind(this)} />
+              <Snippets
+                onClick={this.setCode.bind(this)}
+                types={types[this.state.framework]}
+              />
+              <footer id="footer">{this.state.footerMsg}</footer>
+            </div>
+            <>
+              <Highlight
+                className="prism-code"
+                {...defaultProps}
+                code={this.state.code}
+                theme={dracula}
+                language="javascript">
+                {({
+                  className,
+                  style,
+                  tokens,
+                  getLineProps,
+                  getTokenProps
+                }) => (
+                  <pre className={className} style={style}>
+                    {tokens.map((line, i) => (
+                      <div {...getLineProps({ line, key: i })}>
+                        {line.map((token, key) => (
+                          <span {...getTokenProps({ token, key })} />
+                        ))}
+                      </div>
+                    ))}
+                  </pre>
+                )}
+              </Highlight>
+            </>
           </div>
-          <>
-            <Highlight
-              className="prism-code"
-              {...defaultProps}
-              code={this.state.code}
-              theme={dracula}
-              language="javascript">
-              {({ className, style, tokens, getLineProps, getTokenProps }) => (
-                <pre className={className} style={style}>
-                  {tokens.map((line, i) => (
-                    <div {...getLineProps({ line, key: i })}>
-                      {line.map((token, key) => (
-                        <span {...getTokenProps({ token, key })} />
-                      ))}
-                    </div>
-                  ))}
-                </pre>
-              )}
-            </Highlight>
-          </>
         </div>
-      </div>
+      </SnptProvider>
     );
   }
 }
