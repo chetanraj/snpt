6b9562dc0afecf1d43a28a454b034c1e5a9b5e21
Chetan Raj <chetan.raj@active.ai>
2019-10-17 23:07:05 +0530

    Changes: Added color for Libraries/Frameworks & converted CSS to SCSS for some

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,9 @@ export default class Snpt extends Component {
                   return (
                     <li
                       className={
-                        props.framework === __framework ? 'active' : ''
+                        props.framework === __framework
+                          ? `${__framework} active`
+                          : __framework
                       }
                       onClick={() => this.props.onClick(__framework)}
                       key={__framework}>
